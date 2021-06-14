364a11bc24c812bb0430284c183621adf02a3bfd
shiva552 <shivadantare@gmail.com>
2021-06-14 14:47:58 +0530

    pythonemail

---
+++ email.notes.py
@@ -0,0 +1,10 @@
+"""Transport Layer Security (TLS)
+TLS is a protocol that encrypts and delivers mail securely, for
+and outbound mail traffic. It helps prevent eavesdropping between mail servers –
+keeping your messages private while they're moving between email providers. 
+TLS is being adopted as the standard for secure email"""
+
+"""SMTP stands for Simple Mail Transfer Protocol. SMTP is a set of communication guidelines
+ that allow software to transmit an electronic mail over the internet is called Simple Mail 
+ Transfer Protocol.
+ It is a program used for sending messages to other computer users based on e-mail addresses."""
---
+++ email.send.py
@@ -0,0 +1,22 @@
+import smtplib as s  #inbuilt already
+
+ob = s.SMTP("smtp.gmail.com",587)   #making an object
+#587 :code of gmail for sending
+
+ob.starttls()   #this is a method for sending
+ob.login("shivadantare@gmail.com","Shiva@2001")  #first it will login our account
+subject= "Sending Email Using Python"  #subject for mail
+body="Hello my name is Shiva. How are you doing?"
+message ="Subject:{}\n\n{}".format(subject,body)   #keeping subject and body together as a msg
+listofaddress=["shivadantare@gmail.com","yogeshdantare@gmail.com"]
+ob.sendmail("shivadantare",listofaddress,message)  #defining id from which we want to send mail
+print("Sent Successfully")
+ob.quit()
+
+# MAIL_MAILER=smtp
+# MAIL_HOST=smtp.gmail.com
+# MAIL_PORT=587
+# MAIL_USERNAME=null
+# MAIL_PASSWORD=null
+# MAIL_ENCRYPTION=tls
+
